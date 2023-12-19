96bdbf4811b0f81d6ade945aae76d32b3e92b5c5
Coder731 <stephenobrien1@gmail.com>
2023-12-19 16:59:06 +0000

    Bug Add html8 py
    Copy from
    html7 py
    
    delete lines
    after
    19
    from 20
    onwards
    
    Bug
    stuck in loop

---
+++ html8.py
@@ -0,0 +1,19 @@
+long_string="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+import xmltojson 
+import json 
+import requests
+import pandas
+import re
+import time
+import nltk
+time3=time.monotonic() # https://docs.python.org/3/library/time.html
+url = "http://127.0.0.1:5500/"
+headers = {'User-Agent': long_string } 
+html_response = requests.get(url=url, headers = headers) 
+with open("index.html", "r", encoding='ISO-8859-1') as html_file: 
+    for i in html_file:
+        tokens = nltk.word_tokenize(i) # https://www.nltk.org/
+        for token in tokens:
+            print("line 16: tokens=", token)
+            for token_char in token:
+                list_of_tags=[]
