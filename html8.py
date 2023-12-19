4b5c1e29d35244f6aa935da744f8dedfb262028d
Coder731 <stephenobrien1@gmail.com>
2023-12-19 18:57:47 +0000

    html8 py Cut some commented code

--- html8.py
+++ html8.py
@@ -19,23 +19,9 @@ with open("index.html", "r", encoding='ISO-8859-1') as html_file:
         for index4, i4 in enumerate(i):
             i4new=i4.translate(translation_table)
             # print(i4new)
-            # if ("<" not in i4 and ">" not in i4 and "/" not in i4 and "osano" not in i4):
-            # print(i)
             tokens = nltk.word_tokenize(i) # https://www.nltk.org/
             tagged = nltk.pos_tag(tokens) # new
-            # for index2, token in enumerate(tokens):
-                # print(token)
             for index3, x in enumerate(tagged):
                 [one_tag, tag_type]=x
                 if tag_type=='NN':
-                    # if ("=" not in one_tag and ">" not in one_tag and "comma-separator" not in one_tag and "a2" not in one_tag and "_Ctrl" not in one_tag and "a1" not in one_tag):
-                    #     if ("a4" not in one_tag and "a5" not in one_tag and "<" not in one_tag):
-                    #         if ("mathstat" not in one_tag and "bse" not in one_tag and "div" not in one_tag):
-                    #             if ("%" not in one_tag and "amp" not in one_tag):
                     print(one_tag)
-
-# "<"  ">"  "/" "osano"
-# "="  ">" "comma-separator" "a2" "_Ctrl"  "a1"
-# "a4"  "a5" "<" 
-# "mathstat" "bse" "div"
-# "%"  "amp"
