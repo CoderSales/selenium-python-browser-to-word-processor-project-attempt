7e5fd57e27b25f2bb4cb4bdddcfdf7c4b350bc43
Coder731 <stephenobrien1@gmail.com>
2023-12-19 17:49:33 +0000

    Cut commented code

--- html8.py
+++ html8.py
@@ -14,8 +14,4 @@ with open("index.html", "r", encoding='ISO-8859-1') as html_file:
     for index, i in enumerate(html_file):
         tokens = nltk.word_tokenize(i) # https://www.nltk.org/
         for index2, token in enumerate(tokens):
-            # print("line 17: tokens=", token)
-            # for index3, token_char in enumerate(token):
-            #     if token_char!="'":
-                    # print(token_char)
             print(token)
